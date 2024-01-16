6584ee3174b9e04ec569e34d198625d0cb08faa3
Josh Macklin <69019114+JoshuaMacklin@users.noreply.github.com>
2024-01-16 05:12:18 +0000

    Fix Flags

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-FLAGS=''
+EXTRA_FLAGS=()
 
 # Display Socket
-if [ "$XDG_SESSION_TYPE" = "wayland" ] && [ -e "${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY:-wayland-0}" ]; then
-    FLAGS="$FLAGS --ozone-platform-hint=auto --enable-features=WaylandWindowDecorations"
+if [ "${XDG_SESSION_TYPE}" = "wayland" ] && [ -e "${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY:-wayland-0}" ]; then
+    EXTRA_FLAGS+=(
+        "--enable-features=WaylandWindowDecorations"
+        "--ozone-platform-hint=auto"
+    )
 fi
 
-env TMPDIR="$XDG_RUNTIME_DIR/app/${FLATPAK_ID:-io.freetubeapp.FreeTube}" zypak-wrapper /app/freetube/freetube "$FLAGS" "$@"
+export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}" 
+exec zypak-wrapper /app/freetube/freetube "${EXTRA_FLAGS[@]}" "$@"
