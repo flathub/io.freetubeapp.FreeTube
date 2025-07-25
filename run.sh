66b55b000ff306af48b6a96639661ed1c5e82d69
efb4f5ff-1298-471a-8973-3d47447115dc <73130443+efb4f5ff-1298-471a-8973-3d47447115dc@users.noreply.github.com>
2025-07-25 23:22:59 +0200

    remove WaylandWindowDecorations
    Co-Authored-By: absidue <48293849+absidue@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -5,7 +5,6 @@ EXTRA_FLAGS=()
 # Display Socket
 if [ "${XDG_SESSION_TYPE}" = "wayland" ] && [ -e "${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY:-wayland-0}" -o -e "${WAYLAND_DISPLAY}" ]; then
     EXTRA_FLAGS+=(
-        "--enable-features=WaylandWindowDecorations"
         "--ozone-platform-hint=auto"
     )
 fi
