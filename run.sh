10dd85cd629b035efb60a85b95a28d6d415eaa06
efb4f5ff-1298-471a-8973-3d47447115dc <73130443+efb4f5ff-1298-471a-8973-3d47447115dc@users.noreply.github.com>
2025-09-16 21:41:09 +0200

    cleanup code from run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,2 @@
-#!/bin/sh
-
-EXTRA_FLAGS=()
-
-# Display Socket
-if [ "${XDG_SESSION_TYPE}" = "wayland" ] && [ -e "${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY:-wayland-0}" -o -e "${WAYLAND_DISPLAY}" ]; then
-    EXTRA_FLAGS+=(
-        "--ozone-platform-hint=auto"
-    )
-fi
-
 export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}" 
-exec zypak-wrapper /app/freetube/freetube "${EXTRA_FLAGS[@]}" "$@"
+exec zypak-wrapper /app/freetube/freetube "$@"
